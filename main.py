d87415b3449e7cd1940dd5b9f3baa56352cdc873
Andrei-Eduard Constantinescu <andreiec112@gmail.com>
2021-02-22 18:54:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ def barcode_reader(frame):
 
     # Decodes QR / Barcodes from frame
     barcodes = pyzbar.decode(frame)
-    cv2.putText(frame, "Recognized text: ", (10, 20), cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), 1)
+
+    cv2.rectangle(frame, (9, 5), (185, 23), (0, 0, 0), -1) # Black background for text
+    cv2.putText(frame, "Recognized text: ", (10, 20), cv2.FONT_HERSHEY_PLAIN, 1.2, (255, 255, 255), 1)  # White text
+
+    number_of_barcodes = 0
 
     for barcode in barcodes:
         x, y, w, h = barcode.rect
@@ -16,7 +20,9 @@ def barcode_reader(frame):
 
         # Put a rectangle at the detected code and displays decoded text
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(frame, barcode_info, (10, 40), cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 0, 255), 1)
+        cv2.putText(frame, barcode_info, (10, 40 + 20 * number_of_barcodes), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 255, 0), 1)
+
+        number_of_barcodes += 1
 
     return frame
 
